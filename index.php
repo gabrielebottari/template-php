95d8be40bc437491fc3bfd9e3713af63e11ab470
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-07 13:22:41 +0100

    ADD template

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    echo 'Template PHP';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Template PHP</title>
+    </head>
+    <body>
+
+        <header>
+            HEADER
+        </header>
+
+        <main>
+            MAIN
+        </main>
+
+        <footer>
+            FOOTER
+        </footer>
+        
+    </body>
+</html>
